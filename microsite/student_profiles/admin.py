ab120f3074a1c225f466eacf5a258e99e4bd3297
ib565 <ish.bhartiya@gmail.com>
2024-04-23 01:02:56 +0530

    Add Employer and Opportunity models to admin.py

--- microsite/student_profiles/admin.py
+++ microsite/student_profiles/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Student
+from .models import Student, Employer, Opportunity
 
 admin.site.register(Student)
+admin.site.register(Employer)
+admin.site.register(Opportunity)
