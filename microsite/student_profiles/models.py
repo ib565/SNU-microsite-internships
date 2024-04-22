ec6783f204ddcbfa30120825efad25baac967842
ib565 <ish.bhartiya@gmail.com>
2024-04-23 01:02:49 +0530

    Add Employer and Opportunity models

--- microsite/student_profiles/models.py
+++ microsite/student_profiles/models.py
@@ -48,3 +48,27 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+
+class Employer(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=255, blank=True, null=True, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Opportunity(models.Model):
+    title = models.CharField(max_length=255)
+    employer = models.ForeignKey(Employer, on_delete=models.CASCADE, related_name='opportunities')
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    area_of_interest = models.ForeignKey(AreaOfInterest, on_delete=models.SET_NULL, related_name='opportunities', null=True)
+    skills_required = models.ManyToManyField(Skill, related_name='opportunities', blank=True)
+    is_open = models.BooleanField(default=True)
+    posted_on = models.DateField(auto_now_add=True)
+    last_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.title} by {self.employer.name}"
